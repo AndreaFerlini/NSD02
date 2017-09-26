00f693d67aa3fdcb77b278176093198a8f448f95
AndreaFerlini <andrea.ferlini@fcssolutions.it>
2017-09-26 16:46:26 +0200

    init practical 2 repo

---
+++ cmake-build-debug/CMakeFiles/Practical2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practical2.dir/main.cpp.o"
+  "Practical2.pdb"
+  "Practical2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practical2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
