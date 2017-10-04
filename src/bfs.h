c87e3666bd3c5c95716323a493bb9379346842d4
AndreaFerlini <andrea.ferlini@fcssolutions.it>
2017-10-04 17:54:53 +0200

    fixed bug header bfs

--- src/bfs.h
+++ src/bfs.h
@@ -10,7 +10,7 @@
 /// FUNCTIONS
 
 
-int executeBFS(unsigned int source, AdjacencyList &adjGraph, unsigned int *distance_array, bool debug = false, );
+int executeBFS(unsigned int source, AdjacencyList &adjGraph, unsigned int *distance_array, bool debug = false);
 
 unsigned int estimateDiameter(AdjacencyList &adjGraph, int convergence_th, bool debug = false);
 #endif //PRACTICAL2_BFS_H
